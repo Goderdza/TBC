05ded1a5c04137e27d2acb418193994fa74b11e5
gagva <goderdzi.valishvili.1@btu.edu.ge>
2024-07-21 20:58:42 +0400

    fixed header

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("concept_link")
+    .addEventListener("click", function () {
+      window.location.href = "https://tbcconcept.ge/ge";
+    });
+});
